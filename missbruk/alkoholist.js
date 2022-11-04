49c8a9dc03a8d272d7d6f7c3b14766fb17bfc6c6
antonnyst <35500074+antonnyst@users.noreply.github.com>
2022-11-04 20:25:00 +0100

    Work on apk

--- missbruk/alkoholist.js
+++ missbruk/alkoholist.js
@@ -1,6 +1,5 @@
-
 let checkDone = (element) => {
-    return element.style.border == "5px solid red"
+    return element.processed == true;
 }
 
 let getData = (root) => {
@@ -14,7 +13,7 @@ let getData = (root) => {
     
     volume = volume.slice(0, -3) // milliliter
     percentage = percentage.slice(0, -2).replace(",",".") / 100 // procent
-    price = price.replace(/\*|:-/g,"").replace(":",".") // kr
+    price = price.replace(/\ |\*|:-/g,"").replace(":",".") // kr
 
     return [
         parseFloat(volume), 
@@ -23,28 +22,71 @@ let getData = (root) => {
     ]
 }
 
-let calculate = (element) => {
-    [volume, percentage, price] = getData(element)
-    
-    let apk = volume * percentage / price
-    
-    let vol_perc_element = element.childNodes[0].childNodes.length == 2 ?
-       element.childNodes[0].childNodes[1] :
-       element.childNodes[0].childNodes[0] 
+let apkTemplate = 
+    `<div style="display: flex">
+        <div height=\"24\" class=\"css-3qi0zm e1fve3pg0\" style="background-color: {COLOR}">
+            <p color=\"green500\" class=\"css-15hgqif e1fve3pg1\" style="color: {TCOLOR}">
+                APK: {APK}
+            </p>
+        </div>
+    </div>`;
 
-    vol_perc_element.childNodes[2].textContent += " APK: " + apk.toFixed(3)
+let apkToColor = (apk) => {
+    if (apk > 2) {
+        return ["#5caa7f", "#ffffff"];
+    } else {
+        return ["cdead5", "#095741"];
+    }
 }
 
-let runner = () => {
-    console.log("Running")
+let calculate = (element) => {
+    [volume, percentage, price] = getData(element);
+    console.log(volume, percentage, price);
+    let apk = volume * percentage / price;
+    
+    let template = document.createElement('template');
+    [color, tcolor] = apkToColor(apk)
+    template.innerHTML = apkTemplate
+        .replace("{APK}", apk.toFixed(3))
+        .replace("{COLOR}", color)
+        .replace("{TCOLOR}", tcolor);
+    let apkElement = template.content.firstChild;
+    
+    element.parentNode.insertBefore(
+        apkElement, 
+        element.parentNode.children[2]
+        );
+}
+
+let sortiment = () => {
     let all = document.getElementsByClassName("css-1d9u16r e3whs8q0")
     for(let element of all) {
         if (!checkDone(element)) {
             console.log(element);
             calculate(element)
-            element.style.border = "5px solid red";
+            element.processed = true;
         }
     }
 }
 
-setInterval(runner,1000)
+let produkt = () => {
+
+}
+
+let runner = () => {
+    let location = window.location.href.match(/produkt|sortiment/)
+    if (location != null) {
+        location = location[0]
+    }
+
+    switch (location) {
+        case "sortiment":
+            sortiment();
+            break;
+        case "produkt":
+            produkt();
+            break;
+    }
+}
+
+setInterval(runner,500)
