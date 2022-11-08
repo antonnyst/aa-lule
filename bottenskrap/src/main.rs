7828b5af9191b1c641baff0cac65c0e4b9f7bed4
antonnyst <35500074+antonnyst@users.noreply.github.com>
2022-11-08 19:26:05 +0100

    Start work on mongodb integration

--- bottenskrap/src/main.rs
+++ bottenskrap/src/main.rs
@@ -1,4 +1,5 @@
 use serde_json::Value;
+use mongodb::{bson::doc, options::ClientOptions, Client};
 
 async fn get_page(page: u32) -> Result<Value, http_types::Error>  {
     let req = format!(
@@ -24,7 +25,8 @@ async fn get_all_pages() {
         println!("{}{}", i, page["metadata"]);
         let next_page = page["metadata"]["nextPage"].as_i64().unwrap();
         for product in page["products"].as_array().unwrap() {
-            println!("{}", product["productNameBold"]);
+            save(product).await;
+            println!("{}", product);
         }
         if next_page == -1 {
             break;
@@ -33,9 +35,41 @@ async fn get_all_pages() {
     }
 }
 
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn save(element: &Value) {
+    let name = element["productNameBold"].as_str().unwrap();
+    let productNumber = element["productNumber"].as_i64().unwrap();
+    let price = element["price"].as_i64().unwrap();
+    let volume = element["volume"].as_i64().unwrap();
+    let percentage = element["alcoholPercentage"].as_i64().unwrap();
+    let apk = (percentage / 100) * volume / price;
+}
 
-    get_all_pages().await
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()>{
+    println!("Hello, world!");
+    // Parse your connection string into an options struct
+    /*let mut client_options = ClientOptions::parse("mongodb+srv://<username>:<password>@<cluster-url>/test?w=majority")
+        .await?;
+    
+    // Manually set an option
+    client_options.app_name = Some("Rust Demo".to_string());
+    
+    // Get a handle to the cluster
+    let client = Client::with_options(client_options)?;
+    
+    // Ping the server to see if you can connect to the cluster
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await?;
+    println!("Connected successfully.");
+    
+    // List the names of the databases in that cluster
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }*/
+
+    get_all_pages().await;
+
+    Ok(())
 }
