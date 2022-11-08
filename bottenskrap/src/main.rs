ca4a309bc72ed000e2a8cb0d164520b69392bb95
antonnyst <35500074+antonnyst@users.noreply.github.com>
2022-11-08 17:16:57 +0100

    Move to tokio

--- bottenskrap/src/main.rs
+++ bottenskrap/src/main.rs
@@ -1,12 +1,11 @@
-use futures::executor::block_on;
 use serde_json::Value;
 
 async fn get_page(page: u32) -> Result<Value, http_types::Error>  {
     let req = format!(
-        "https://www.systembolaget.se/api/gateway/productsearch/search/
-        ?size=30
-        &storeId=2520
-        &isInStoreAssortmentSearch=true
+        "https://www.systembolaget.se/api/gateway/productsearch/search/\
+        ?size=30\
+        &storeId=2520\
+        &isInStoreAssortmentSearch=true\
         &page={}", 
         page.to_string()
     );
@@ -14,27 +13,29 @@ async fn get_page(page: u32) -> Result<Value, http_types::Error>  {
     let mut res = surf::get(req)
     .header("baseUrl", "https://api-systembolaget.azure-api.net/sb-api-ecommerce/v1").await?;
     let string = res.body_string().await?;
+
     Ok(serde_json::from_str(&string).unwrap())
 }
 
-fn get_all_pages() {
+async fn get_all_pages() {
     let mut i = 1;
     loop {
-        let page = block_on(get_page(i)).unwrap();
+        let page = get_page(i).await.unwrap();
         println!("{}{}", i, page["metadata"]);
         let next_page = page["metadata"]["nextPage"].as_i64().unwrap();
         for product in page["products"].as_array().unwrap() {
             println!("{}", product["productNameBold"]);
         }
-        if next_page == -1{
+        if next_page == -1 {
             break;
         }
         i += 1;
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 
-    get_all_pages()
+    get_all_pages().await
 }
