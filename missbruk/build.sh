f60482972dfdf2c3c093ead7ecc803d4018fe403
antonnyst <35500074+antonnyst@users.noreply.github.com>
2023-02-02 10:51:06 +0100

    Add firefox and chrome build setup

---
+++ missbruk/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Creates a zip of relevant files 
+# and chooses manifest based on argument 
+
+case $1 in 
+    firefox)
+        cp manifest_v2.json manifest.json
+        ;;
+    chrome)
+        cp manifest_v3.json manifest.json
+        ;;
+    *)
+        echo "Invalid argument"
+        exit
+        ;;
+esac
+
+zip -r missbruk.zip manifest.json code/ icons/
+
+rm manifest.json
